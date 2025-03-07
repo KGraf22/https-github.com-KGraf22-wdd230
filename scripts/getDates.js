38baa35e2c0a0f27679ff0c23dc76e16f60c0142
KGraf22 <144725954+KGraf22@users.noreply.github.com>
2025-03-07 11:46:33 -0800

    Course Home Page

---
+++ scripts/getDates.js
@@ -0,0 +1,5 @@
+//current year in the footer
+document.getElementById('currentYear').textContent = new Date().getFullYear();
+
+//last modified date in the footer
+document.getElementById('lastModified').textContent = 'Last Modified: ' + document.lastModified;
